0c190d836a78677b7e44ec585e910d8c854ed65d
logidhasan <logidhasan25@gmail.com>
2026-05-05 12:14:09 +0530

    non stream response

---
+++ claude/main.py
@@ -0,0 +1,10 @@
+from .client.llm_client import LLMClient
+import asyncio
+
+async def main():
+    client = LLMClient()
+    messages = [{"role": "user", "content": "Hello"}]
+    await client.chat_completion(messages, False)
+    print("done")
+
+asyncio.run(main())
